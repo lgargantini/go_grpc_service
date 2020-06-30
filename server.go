878ebab1248c45b571a36cdb9c74c33fafe10e94
Marcelo Romagnoli <marce.romagnoli@gmail.com>
2020-06-30 21:15:54 +0200

    Refactor Devide for test passing

--- server.go
+++ server.go
@@ -15,3 +15,8 @@ func (s *server) Add(ctx context.Context, in *pb.Operands) (*pb.Result, error) {
 
 	return &pb.Result{Result: result}, nil
 }
+
+func (s *server) Divide(ctx context.Context, in *pb.Operands) (*pb.Result, error) {
+	result := in.GetFirstOperand() / in.GetSecondOperand()
+	return &pb.Result{Result: result}, nil
+}
