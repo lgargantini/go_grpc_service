8090526bb8502125a94085bdce1a483aa743226d
Marcelo Romagnoli <marce.romagnoli@gmail.com>
2020-07-01 21:11:30 +0200

    Add logic to start server

--- service/server/server.go
+++ service/server/server.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
-	pb "grpc_service/protos"
+	pb "grpc_service/service/protos"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
 )
 
 type server struct {
@@ -24,4 +28,17 @@ func (s *server) Divide(ctx context.Context, in *pb.Operands) (*pb.Result, error
 	result := in.GetFirstOperand() / in.GetSecondOperand()
 
 	return &pb.Result{Result: result}, nil
+}
+
+func main() {
+	listen, err := net.Listen("tcp", ":12000")
+	if err != nil {
+		log.Fatal("failed to listen: %v", err)
+	}
+	svr := grpc.NewServer()
+	pb.RegisterCalculatorServiceServer(svr, &server{})
+	log.Printf("Listening on port %d", 12000)
+	if err := svr.Serve(listen); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
