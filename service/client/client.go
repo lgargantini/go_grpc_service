22fe4c66a7856d5fe6a052aa1f9d8abd7c6fdcf6
Marcelo Romagnoli <marce.romagnoli@gmail.com>
2020-07-07 17:48:37 +0200

    Tidy up a little

--- service/client/client.go
+++ service/client/client.go
@@ -10,19 +10,32 @@ import (
 	"google.golang.org/grpc"
 )
 
-func main() {
+func initialize() (pb.CalculatorServiceClient, context.Context) {
 	conn, err := grpc.Dial("localhost:12000", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("couldn't connect: %v", err)
 	}
+
 	defer conn.Close()
+
+	// Instantiate a new client
 	client := pb.NewCalculatorServiceClient(conn)
 
+	// Generate the context
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
 	defer cancel()
+
+	return client, ctx
+}
+
+func main() {
+	client, ctx := initialize()
+
 	response, err := client.Add(ctx, &pb.Operands{FirstOperand: 10, SecondOperand: 20})
 	if err != nil {
 		log.Fatalf("couldn't call method: %v", err)
 	}
+
 	log.Printf("Response: %v", response.GetResult())
 }
