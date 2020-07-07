a6e6dd5c741e4f2e30067c951be1c922d5b4c8a1
Marcelo Romagnoli <marce.romagnoli@gmail.com>
2020-07-07 18:52:19 +0200

    Iterate client to call service by params

--- service/client/client.go
+++ service/client/client.go
@@ -2,37 +2,57 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
+
+	flag "github.com/spf13/pflag"
 
 	pb "grpc.go/service/protos"
 
 	"google.golang.org/grpc"
 )
 
-func initialize() (pb.CalculatorServiceClient, context.Context) {
+func initialize() (pb.CalculatorServiceClient, context.Context, context.CancelFunc, *grpc.ClientConn) {
 	conn, err := grpc.Dial("localhost:12000", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("couldn't connect: %v", err)
 	}
 
-	defer conn.Close()
-
 	// Instantiate a new client
 	client := pb.NewCalculatorServiceClient(conn)
 
 	// Generate the context
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancelContext := context.WithTimeout(context.Background(), 10*time.Second)
 
-	defer cancel()
-
-	return client, ctx
+	return client, ctx, cancelContext, conn
 }
 
 func main() {
-	client, ctx := initialize()
+	action := flag.String("o", "add", "Operation to perform")
+	var defaultFloats []float32
+	operands := flag.Float32Slice("n", defaultFloats, "Operands for the operation")
 
-	response, err := client.Add(ctx, &pb.Operands{FirstOperand: 10, SecondOperand: 20})
+	flag.Parse()
+
+	if len(*operands) != 2 {
+		log.Fatal("operands must be two")
+	}
+
+	fmt.Println((*operands)[0])
+
+	client, ctx, cancelContext, connection := initialize()
+	defer connection.Close()
+	defer cancelContext()
+
+	var response *pb.Result
+	var err error
+
+	switch *action {
+	case "add":
+		response, err = client.Add(ctx, &pb.Operands{FirstOperand: (*operands)[0], SecondOperand: (*operands)[1]})
+	}
+
 	if err != nil {
 		log.Fatalf("couldn't call method: %v", err)
 	}
--- service/go.mod
+++ service/go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/golang/protobuf v1.4.2
+	github.com/spf13/pflag v1.0.5
 	google.golang.org/grpc v1.30.0
 )
